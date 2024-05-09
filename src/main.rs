bdbf44a595d6ee9befed39d8278d7c773449ad8d
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2024-05-09 15:01:57 +0000

    modify output method

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::collections::HashMap;
 use clap::Parser;
 use cli::Cli;
 use reqwest::header::HeaderMap;
-use serde_json::value::Value;
+use tokio::io::{self, AsyncWriteExt};
 
 
 mod cli;
 
-async fn post(cli: &Cli) -> Result<HashMap<String, Value>, reqwest::Error>{
+async fn post(cli: &Cli) -> Result<HashMap<String, Vec<u8>>, reqwest::Error>{
     let url = format!("http://{}:{}/test/{}/{}", cli.backend_address, cli.port, cli.branch, cli.commit_id);
 
     // post 请求要创建client
@@ -17,13 +17,8 @@ async fn post(cli: &Cli) -> Result<HashMap<String, Value>, reqwest::Error>{
     let mut headers = HeaderMap::new();
     headers.insert("Content-Type", "application/json".parse().unwrap());
 
-    // 组装要提交的数据
-    let mut data: HashMap<String, String> = HashMap::new();
-    // data.insert("user", "tangjz");
-    // data.insert("password", "dev-tang.com");
-
     // 发起post请求并返回
-    Ok(client.post(url).headers(headers).json(&data).send().await?.json::<HashMap<String, Value>>().await?)
+    Ok(client.post(url).headers(headers).send().await?.json::<HashMap<String, Vec<u8>>>().await?)
 }
 
 #[tokio::main]
@@ -32,7 +27,10 @@ async fn main() {
 
 
     match post(&cli).await {
-        Ok(res) => println!("{:#?}", res),
+        Ok(res) => {
+            io::stdout().write_all(&res["func_output"]).await.unwrap();
+            io::stdout().write_all(&res["perf_output"]).await.unwrap();
+        },
         Err(e) => println!("{:#?}", e),
     }
 }
