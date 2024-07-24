8bfc61f92e56f058eca7c8c25ac6f9c350da93dc
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2024-07-24 13:23:28 +0000

    能在测试失败时panic

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use std::collections::HashMap;
 use clap::Parser;
 use cli::Cli;
 use reqwest_eventsource::{Event, EventSource};
-use tokio::io::{self, AsyncWriteExt};
 use futures::stream::StreamExt;
+use anyhow::{Error, Result};
 mod cli;
 
-async fn post(cli: &Cli) -> Result<HashMap<String, Vec<u8>>, reqwest::Error>{
+async fn post(cli: &Cli) -> Result<()>{
     let mut data = HashMap::new();
     data.insert("branch".to_string(), cli.branch.clone());
     data.insert("commit_id".to_string(), cli.commit_id.clone());
@@ -17,26 +17,39 @@ async fn post(cli: &Cli) -> Result<HashMap<String, Vec<u8>>, reqwest::Error>{
         .post(url)
         .json(&data);
     let mut es = EventSource::new(response).expect("Failed to create EventSource");
+    let mut failed = false;
     while let Some(event) = es.next().await {
         match event {
             Ok(Event::Open) => println!("Connection Open!"),
             Ok(Event::Message(message)) => {
-                println!("{}", message.data); // not sure how to send this messages to user
+                if message.data == "Exit Code: 1"{
+                    failed = true;
+                }
+                println!("{}", message.data);
             }
             Err(err) => {
-                println!("Sse Error: {}", err);
+                if let reqwest_eventsource::Error::StreamEnded = err {
+                    println!("{}", err);
+                }else{
+                    println!("Sse Error: {}", err);
+                    failed = true;
+                }
                 es.close();
             }
         }
     }
 
+    if failed {
+        return Err(Error::msg(""));
+    }
+
     // 发起post请求并返回
     // Ok(client.post(url).headers(headers).send().await?.json::<HashMap<String, Vec<u8>>>().await?)
-    Ok(HashMap::new())
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
-    post(&cli).await.unwrap();
+    post(&cli).await.expect("Test Failed");
 }
